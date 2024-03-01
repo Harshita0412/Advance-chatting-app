82be9626589b44949214d70790b5fb81a1b26275
Creative Harshita <115688367+Harshita0412@users.noreply.github.com>
2024-03-01 19:52:59 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,21 @@
+ const socket = io('https://localhost:8000');
+
+ const form = document.getElementById('send-container');
+ const messageInput = document.getElementById('messageInp')
+ const messageContainer = document.querySelector(".container")
+
+ const name = prompt("Enter your name to join");
+ socket.emit('new-user-joined', name );
+
+
+
+
+ 
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,18 @@
+//Node server which will handle socket io connections
+
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection' , socket =>{
+    socket.on('new-user-joined', name =>{
+        console.log("New user", name)
+        user[socket.id] = name;
+        socket.broadcast.emit('user joined', name);
+
+    })
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {message: message, name: user[socket.id] })
+
+    });
+})
